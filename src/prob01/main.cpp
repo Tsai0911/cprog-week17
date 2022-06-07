cfc147b1e94551bcf69508c4dd690b17eb5236a4
Tsai0911 <100834751+Tsai0911@users.noreply.github.com>
2022-06-07 15:14:25 +0800

    Update main.cpp

--- src/prob01/main.cpp
+++ src/prob01/main.cpp
@@ -9,3 +9,18 @@ using namespace std;
 
 // 1. 你所寫的程式碼只能在這個段落。
 // 2. 請依照題目說明完成欠缺的程式碼。
+
+int main()
+{
+    int n;
+    int m;
+    cin >> n >> m;
+    for (int i = 1; i <= n;i ++)
+    {
+        if ( i % m == 0)
+        cout << setw(4) << i;
+    }
+    cout << endl;
+
+    return 0;
+}
